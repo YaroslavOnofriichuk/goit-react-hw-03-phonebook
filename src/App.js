3c27758b7dbc13a3cbb9d1e8160427327cd7959f
YaroslavOnofriichuk <YaroslavOnofriichuk@ukr.net>
2022-01-30 16:18:31 +0200

    reworked hw 03

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import Section from "./components/Section.styled";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: "",
     filter: "",
   };
 
@@ -22,6 +17,7 @@ class App extends Component {
       this.setState({ contacts });
     } catch {
       console.error();
+      this.setState({ contacts: ""});
     }
   };
 
@@ -43,12 +39,15 @@ class App extends Component {
       id: nanoid(),
     };
 
-    if (
+    if (!this.state.contacts) {
+      this.setState({
+          contacts: [newContact],
+      });
+    } else if (
       this.state.contacts.find(
         (contact) =>
           contact.name.toLowerCase() === newContact.name.toLowerCase()
-      )
-    ) {
+      )) {
       window.alert(`${newContact.name} is already in contacts`);
     } else {
       this.setState((prevState) => {
@@ -67,9 +66,13 @@ class App extends Component {
   };
 
   filterContact = () => {
-    return this.state.contacts.filter((contact) => {
-      return contact.name.toLowerCase().includes(this.state.filter);
-    });
+    try {
+      return this.state.contacts.filter((contact) => {
+        return contact.name.toLowerCase().includes(this.state.filter);
+      });
+    } catch { 
+      return false;
+    }
   };
 
   handleDelete = (e) => {
@@ -89,10 +92,11 @@ class App extends Component {
         <ContactForm onSubmit={this.handleSubmit} htmlFor={this.formId} />
         <p>Contacts</p>
         <Filter onChange={this.handleSearch} />
+        {this.filterContact() && 
         <ContactList
           contacts={this.filterContact()}
           onClick={this.handleDelete}
-        />
+        />}
       </Section>
     );
   }
